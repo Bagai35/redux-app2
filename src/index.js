fa848e180836be38034afa9b7c61513d20a9f8f8
Bogdan Keplast <bogdan.keplast@gmail.com>
2023-10-13 17:02:06 +0300

    add ZERO

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { createStore, bindActionCreators } from 'redux';
+import reducer from './reducer';
+import * as actions from './actions'; // Import action creators
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(reducer);
+const { dispatch, subscribe } = store;
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState().value;
+};
+subscribe(update);
+
+const { inc, dec, rnd, pls, mns, zero } = bindActionCreators(actions, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('pls').addEventListener('click', pls);
+document.getElementById('mns').addEventListener('click', mns);
+document.getElementById('zero').addEventListener('click', zero);
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    rnd(value);
+});
+
+ReactDOM.render(
+    <React.StrictMode>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
