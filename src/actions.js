e23bd07f3f847865cc826219fbd67be23334b33e
Bogdan Keplast <bogdan.keplast@gmail.com>
2023-10-23 09:12:15 +0300

    SCRPT-13

--- src/actions.js
+++
@@ -1,7 +0,0 @@
-export const inc = () => ({type: 'INC'});
-export const dec = () => ({type: 'DEC'});
-export const pls = () => ({type: 'PLS'});
-export const mns = () => ({type: 'MNS'});
-export const zero = () =>({type: 'ZERO'})
-export const rnd = (value) => ({type: 'RND', payload: value});
-
--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
-import reducer from './reducer';
-import * as actions from './actions'; // Import action creators
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-const store = createStore(reducer);
-const { dispatch, subscribe } = store;
+import App from './components/app/App';
+import store from './store';
 
-const update = () => {
-    document.getElementById('counter').textContent = store.getState().value;
-};
-subscribe(update);
-
-const { inc, dec, rnd, pls, mns, zero } = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('pls').addEventListener('click', pls);
-document.getElementById('mns').addEventListener('click', mns);
-document.getElementById('zero').addEventListener('click', zero);
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
+import './styles/index.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
